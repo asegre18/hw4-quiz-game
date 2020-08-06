5fc40aaa6ba467590fc445bd8b471fbbf09f4c55
Andrew Segre <asegre18@cmc.edu>
2020-08-06 13:28:01 -0700

    pseudo code

---
+++ script.js
@@ -0,0 +1,8 @@
+// psuedo code
+// variables add below
+
+
+// create an object with each multiple choice question and answer
+// add key "q:" "a1", "a2", "a3", "a4" - answers too
+// total of 10 questions with 4 possible answers
+
